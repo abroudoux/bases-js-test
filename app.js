18320fa78adcca0cde38cbba52b9295aaa9c5f3e
arthur <arthur.broudoux@gmail.com>
2024-03-04 15:57:53 +0100

    js + css

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('JS chargé')
+console.log("JS chargé");
+
+let form = document.getElementById("form");
+let message = document.getElementById("message");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let firstName = document.getElementById("firstName").value;
+  let lastName = document.getElementById("lastName").value;
+  let day = document.getElementById("day").value;
+  let month = document.getElementById("month").value;
+  let year = document.getElementById("year").value;
+
+  let infosUser =
+    firstName + " " + lastName + ", né(e) le " + day + "/" + month + "/" + year;
+  console.log(infosUser);
+
+  message.style.display = "block";
+  message.innerHTML =
+    "Merci d'avoir répondu à ce formualire, vos informations sont les suivantes : " +
+    infosUser;
+});
